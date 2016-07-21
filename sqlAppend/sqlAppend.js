8d806b3b6b9176700b97c793cda6ab145c94cc6a
hanwha_park <cain007@naver.com>
2016-07-21 14:28:24 +0900

    호잇
    Change-Id: Id8b6d6780ba5180fea686d8eb7b09de177027765
    Signed-off-by: hanwha_park <cain007@naver.com>

--- sqlAppend/sqlAppend.js
+++ sqlAppend/sqlAppend.js
@@ -1,41 +1,48 @@
-var el = document.getElementById("go");
-
-el.addEventListener("click", function() {
-
-	var lnSql = ''; // 라인 마다 sql
-	var lnSqlArr = []; // 라인 마다 sql 저장 배열
-	var lnSqlLenArr = []; // 라인 마다 sql 길이 저장 배열
-	var lnBlankFlag = false;
-
-	var sql = document.getElementById("sqlFormat").value;
-	sql.split("\n").forEach(function(item, i) {
-
-		lnSql = item.replace(/\s+$/gi, "");
-
-		// 문자열이 없으면 넘김
-		lnBlankFlag = item.replace(/^\s+/gi, "").match(/^(?!\d).*$/);
-		if (!lnBlankFlag) {
-			continue;
-		}
-
-		lnSqlLenArr.push(lnSql.length);
-		lnSqlArr.push(lnSql);
-
-	}); 
-
-	// 각 sql 라인에 최대 길이 정렬하여 얻음
-	var max = lnSqlLenArr.sort(function(a, b) {
-		return b - a;
-	})[0] + 5;
-
-	lnSqlArr.forEach(function(item, i) {
-		var rv = document.getElementById("result").value;
-		document.getElementById("result").value = rv + "sql.append(\"\\n "
-				+ RPAD(item, " ", max) + "\");\n";
-
+/**
+ * 
+ */
+$(document).ready(function(){
+	var el = $("#go");
+	
+	el.click(function() {
+		var lnSql = ''; 			// 라인 마다 sql
+		var lnSqlArr = []; 			// 라인 마다 sql 저장 배열
+		var lnSqlLenArr = []; 		// 라인 마다 sql 길이 저장 배열
+		var lnBlankFlag = false;
+		
+		
+		var sql = $("#sqlFormat").val();
+		
+		$.each(sql.split("\n"), function(item, i) {
+			
+			lnSql = item.replace(/\s+$/gi, "");
+			
+			// 문자열이 없으면 넘김
+//			lnBlankFlag = item.replace(/^\s+/gi, "").test(/^(?!\d).*$/);
+//			if (!lnBlankFlag) {
+//				continue;
+//			}
+			
+			lnSqlLenArr.push(lnSql.length);
+			lnSqlArr.push(lnSql);
+			
+		}); 
+		
+		// 각 sql 라인에 최대 길이 정렬하여 얻음
+		var max = lnSqlLenArr.sort(function(a, b) {
+			return b - a;
+		})[0] + 5;
+		
+		
+		$.each(lnSqlArr, function(item, i) {
+			var rv = $("#result").val();
+			$("#result").val( rv + "sql.append(\"\\n " + RPAD(item, " ", max) + "\");\n" );
+		});
+		
 	});
-
+	
 });
+
 
 // http://hellowworld.tistory.com/entry/자바스크립트로-LPAD-RPAD-구현하기
 function RPAD(s, c, n) {
